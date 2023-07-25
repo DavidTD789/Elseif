9637065b7469296d16e7dd157319ba1f3bc5a232
DavidTD789 <138784445+DavidTD789@users.noreply.github.com>
2023-07-25 13:31:46 +0400

    elseif

---
+++ elseif.js
@@ -0,0 +1,21 @@
+let g = prompt("Enter week day - ")
+if(g == "1"){
+    console.log("erkushapti")
+}
+else if(g == "2"){
+    console.log("erekshapti")
+}else if(g == "3"){
+    console.log("chereshapti")
+}
+else if(g == "5"){
+    console.log("urpat")
+}
+else if(g == "6"){
+    console.log("shapat")
+}
+else if(g == "7"){
+    console.log("kiraki")
+}
+else{
+    console.log("error")
+}
